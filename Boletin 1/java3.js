cd8f9a5aa3607f48fd6c0e21f953d994b37d8af1
anxo <anxoportela@gmail.com>
2019-05-01 12:56:58 +0200

    Aplicar Beautify

--- Boletin 1/java3.js
+++ Boletin 1/java3.js
@@ -1,5 +1,5 @@
 var num = prompt("Introduce un número", "0");
-  if (num != null) {
-    document.getElementById("num").innerHTML =
+if (num != null) {
+  document.getElementById("num").innerHTML =
     "Escogiste el número " + num;
 }
